e848ef3d4f1f88f3657d6d454c2cc1e1cce7a5a3
Subham Maity <97989643+Subham-Maity@users.noreply.github.com>
2022-03-17 05:45:25 +0530

    adding sql files

---
+++ 1. Student Table Create.sql
@@ -0,0 +1,7 @@
+CREATE table "STUDENT" (
+    "ROLL_NUMBER" NUMBER,
+    "NAME"        VARCHAR2(30),
+    "AGE"         NUMBER,
+    "GENDER"      VARCHAR2(8),
+    "CITY"        VARCHAR2(10)
+)
---
+++ 13.Alias Table.sql
@@ -0,0 +1,2 @@
+SELECT column_name(s)
+FROM table_name AS alias_name;
